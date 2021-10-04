eb5abb8476ea1e4b354b8d0fc88d8524299088ef
ElvinasCern <85024039+ElvinasCern@users.noreply.github.com>
2021-10-04 23:09:31 +0300

    clean files

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const h1DOM = document.querySelector('h1');
-h1DOM.innerText = 'Labas Vakar!'
-const pDOM = document.querySelector('p');
-pDOM.innerText = 'Siandien grazus oras!';
-const aDOM = document.querySelector('a');
-aDOM.innerText = 'Spausk mane';
