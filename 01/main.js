8a57b64b1c0d0c5864ee0a20f36816aad8c2d38b
ElvinasCern <85024039+ElvinasCern@users.noreply.github.com>
2021-10-04 19:35:58 +0300

    first task update

--- 01/main.js
+++ 01/main.js
@@ -1,5 +1,5 @@
 const h1DOM = document.querySelector('h1');
-h1DOM.innerText = 'Labas Vakar!'
+h1DOM.innerText = 'Labas vakaras!';
 const pDOM = document.querySelector('p');
 pDOM.innerText = 'Siandien grazus oras!';
 const aDOM = document.querySelector('a');
