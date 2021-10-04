2e1412d16461f4e80fd7701baddc7b02f8f0a06f
ElvinasCern <85024039+ElvinasCern@users.noreply.github.com>
2021-10-04 21:13:34 +0300

    4 uzduotis

---
+++ 04/main.js
@@ -0,0 +1,10 @@
+const h1DOM = document.querySelector('h1');
+h1DOM.innerText = 'Zuikio darzas';
+
+const liDOM = document.querySelectorAll('li');
+
+for (darzove of liDOM){
+    darzove.innerText = 'Israuta darzove';
+};
+
+//liDOM.forEach(i => i.innerText = 'Israuta darzove' );
