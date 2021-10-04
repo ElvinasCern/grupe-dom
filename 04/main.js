1aff5c439b828e3d10c0c88d60cd044f71f7f437
ElvinasCern <85024039+ElvinasCern@users.noreply.github.com>
2021-10-04 21:19:55 +0300

    test commit

--- 04/main.js
+++ 04/main.js
@@ -3,8 +3,8 @@ h1DOM.innerText = 'Zuikio darzas';
 
 const liDOM = document.querySelectorAll('li');
 
-for (darzove of liDOM){
-    darzove.innerText = 'Israuta darzove';
-};
+//for (darzove of liDOM){
+//    darzove.innerText = 'Israuta darzove';
+//};
 
-//liDOM.forEach(i => i.innerText = 'Israuta darzove' );
+liDOM.forEach(i => i.innerText = 'Israuta darzove' );
