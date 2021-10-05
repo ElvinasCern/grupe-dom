ec5538ffeb320dad49c5ce75d15ff1808a33f41c
ElvinasCern <85024039+ElvinasCern@users.noreply.github.com>
2021-10-05 19:47:12 +0300

    7 task

---
+++ 07/main.js
@@ -0,0 +1,23 @@
+const minusDOM = document.querySelector('.minus');
+const pliusDOM = document.querySelector('.plus');
+const resetDOM = document.querySelector('.reset');
+const numberDOM = document.querySelector('.number');
+
+let countNumberDom = 0;
+
+minusDOM.addEventListener('click' , function(){
+    numberDOM.innerText = countNumberDom--;
+});
+
+pliusDOM.addEventListener('click' , function(){
+    numberDOM.innerText = countNumberDom++;
+});
+
+resetDOM.addEventListener('click' , function(){
+    numberDOM.innerText = countNumberDom= 0;
+});
+
+
+
+
+
